b8d5feed9847024600b471c6480f5826b2722ef2
ahmed <ahmed000zrelli@gmail.com>
2026-06-29 15:59:42 +0100

    live demo trigger

--- src/rospug_demo/rospug_demo/talker.py
+++ src/rospug_demo/rospug_demo/talker.py
@@ -11,7 +11,7 @@ class Talker(Node):
     """
 
     def __init__(self):
-        super().__init__('rospug_talker') sssssssdddddddddd
+        super().__init__('rospug_talker')
         self.publisher_ = self.create_publisher(String, 'rospug/status', 10)
         self.timer = self.create_timer(1.0, self.timer_callback)
         self.count = 0
